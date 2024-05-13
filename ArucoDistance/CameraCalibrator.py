58853c921024afa4571ab2756326a42dacb2f701
TABuchkin <timur.buchkin@mail.ru>
2024-05-13 16:36:15 +0300

    add server

--- ArucoDistance/CameraCalibrator.py
+++ ArucoDistance/CameraCalibrator.py
@@ -1,12 +1,18 @@
 import numpy as np
 import cv2 as cv
 import glob
- 
+import time
+
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-def calibrateUsingCamera(cameraIndex = 0, dirToStoreGoodCalibrationFrames = None, doLog = False):
-    objp = np.zeros((7*7, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
+
+
+def calibrateUsingCamera(cameraIndex=0, dirToStoreGoodCalibrationFrames=None, doLog=False):
+    gridHeight = 7
+    gridWidth = 6
+    print("Calibrating started please keep chessboard image opposite camera")
+    objp = np.zeros((gridHeight*gridWidth, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:gridHeight, 0:gridWidth].T.reshape(-1, 2)
 
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
@@ -22,20 +28,24 @@ def calibrateUsingCamera(cameraIndex = 0, dirToStoreGoodCalibrationFrames = None
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (7, 7), None)
+        ret, corners = cv.findChessboardCorners(
+            gray, (gridHeight, gridWidth), None)
 
         # If found, add object points, image points (after refining them)
         if ret == True:
             goodFrames += 1
             objpoints.append(objp)
 
-            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            corners2 = cv.cornerSubPix(
+                gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners2)
 
-            if(doLog):
+            if (doLog):
                 print("The image good for calibration")
-            if(dirToStoreGoodCalibrationFrames is not None):
-                cv.imwrite(f"{dirToStoreGoodCalibrationFrames}/{goodFrames}.jpg", img)
+            if (dirToStoreGoodCalibrationFrames is not None):
+                cv.imwrite(
+                    f"{dirToStoreGoodCalibrationFrames}/{goodFrames}.jpg", img)
+            time.sleep(0.5)
 
     if gray is None:
         print("Error in calibrateUsingCamera: Bad images, can't calibrate")
@@ -45,11 +55,12 @@ def calibrateUsingCamera(cameraIndex = 0, dirToStoreGoodCalibrationFrames = None
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
         objpoints, imgpoints, gray.shape[::-1], mtx_fake, dist_fake, None, None, 0, criteria)
     return {
-            "camera_matrix": mtx,
-            "distortion" : dist,
-            "rvecs" : rvecs,
-            "tvecs" : tvecs,
-            }
+        "camera_matrix": mtx,
+        "distortion": dist,
+        "rvecs": rvecs,
+        "tvecs": tvecs,
+    }
+
 
 def calibrateUsingImages(dirWithImages):
     objp = np.zeros((7*7, 3), np.float32)
@@ -72,7 +83,8 @@ def calibrateUsingImages(dirWithImages):
         if ret == True:
             objpoints.append(objp)
 
-            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            corners2 = cv.cornerSubPix(
+                gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners2)
 
     if len(objpoints) == 0 or gray is None:
@@ -83,8 +95,8 @@ def calibrateUsingImages(dirWithImages):
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
         objpoints, imgpoints, gray.shape[::-1], mtx_fake, dist_fake, None, None, 0, criteria)
     return {
-            "camera_matrix": mtx,
-            "distortion" : dist,
-            "rvecs" : rvecs,
-            "tvecs" : tvecs,
-            }
+        "camera_matrix": mtx,
+        "distortion": dist,
+        "rvecs": rvecs,
+        "tvecs": tvecs,
+    }
