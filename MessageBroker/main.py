a0d0129a5525ed667476654b8252d7c4cc79441e
TABuchkin <timur.buchkin@mail.ru>
2024-05-13 16:49:34 +0300

    Merge all in one

--- MessageBroker/main.py
+++ MessageBroker/main.py
@@ -1,10 +1,13 @@
 import zmq
 import json
 
-CONFIG_NAME = "Config.json"
+DEFAULT_CONFIG_PATH = "./Config.json"
 
 def main():
-    with open(CONFIG_NAME, "r") as file:
+    configPath = input(f"Input config path({DEFAULT_CONFIG_PATH} by default): ")
+    if configPath == "":
+        configPath = DEFAULT_CONFIG_PATH
+    with open(configPath, "r") as file:
         config = json.load(file)
 
     print(f"Starting broker with\n \
